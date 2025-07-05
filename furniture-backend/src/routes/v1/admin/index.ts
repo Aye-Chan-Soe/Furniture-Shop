0de767a309d0007f7bc191ede938560ee0ee5200
Aye Chan Soe <ayechansoe.dev@gmail.com>
2025-07-05 22:07:25 +0630

    Post CRUD: Create

--- furniture-backend/src/routes/v1/admin/index.ts
+++ furniture-backend/src/routes/v1/admin/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import { getAllUsers } from "../../../controllers/admin/userController";
 import { setMaintenance } from "../../../controllers/admin/systemController";
+import upload from "../../../middlewares/uploadFile";
+import {
+  createPost,
+  deletePost,
+  updatePost,
+} from "../../../controllers/admin/Post/postController";
 
 const router = express.Router();
 
 router.get("/users", getAllUsers);
 router.post("/maintenance", setMaintenance);
+
+// CRUD for Posts
+router.post("/posts", upload.single("image"), createPost); // Create
+router.patch("/posts", upload.single("image"), updatePost); // Update
+router.delete("/posts", deletePost);
 
 export default router;
