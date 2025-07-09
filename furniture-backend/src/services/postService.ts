61fbc1a044c42d0b7fa6bdcb20a573cde3e250a2
Aye Chan Soe <ayechansoe.dev@gmail.com>
2025-07-09 19:03:53 +0630

    Fixed error

--- furniture-backend/src/services/postService.ts
+++ furniture-backend/src/services/postService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma";
+import { Post, PrismaClient } from "../../generated/prisma";
 
 const prisma = new PrismaClient();
 
@@ -14,7 +14,7 @@ export type PostArgs = {
 };
 
 export const createOnePost = async (postData: PostArgs) => {
-  let data: any = {
+  const data: any = {
     title: postData.title,
     content: postData.content,
     body: postData.body,
@@ -46,4 +46,56 @@ export const createOnePost = async (postData: PostArgs) => {
   }
 
   return prisma.post.create({ data });
+};
+
+export const getPostById = async (id: number) => {
+  return prisma.post.findUnique({
+    where: { id },
+  });
+};
+
+export const updateOnePost = async (postId: number, postData: PostArgs) => {
+  const data: any = {
+    title: postData.title,
+    content: postData.content,
+    body: postData.body,
+    category: {
+      connectOrCreate: {
+        where: { name: postData.category },
+        create: { name: postData.category },
+      },
+    },
+    type: {
+      connectOrCreate: {
+        where: { name: postData.type },
+        create: { name: postData.type },
+      },
+    },
+  };
+
+  if (postData.image) {
+    data.image = postData.image;
+  }
+
+  if (postData.tags && postData.tags.length > 0) {
+    data.tags = {
+      connectOrCreate: postData.tags.map((tagName) => ({
+        where: { name: tagName },
+        create: { name: tagName },
+      })),
+    };
+  }
+
+  return prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data,
+  });
+};
+
+export const deleteOnePost = async (id: number) => {
+  return prisma.post.delete({
+    where: { id },
+  });
 };
