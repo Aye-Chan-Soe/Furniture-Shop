2332e3e38c2076bb707c67724559fece256a5c16
Aye Chan Soe <ayechansoe.dev@gmail.com>
2025-11-01 18:28:28 +0630

    fixed query.ts

--- furniture-frontend/src/api/query.ts
+++ furniture-frontend/src/api/query.ts
@@ -96,7 +96,7 @@ export const productInfiniteQuery = (
     fetchInfiniteProducts({ pageParam, categories, types }),
   placeholderData: keepPreviousData,
   initialPageParam: null,
-  getNextPageParam: (lastPage: { nextCursor: any; posts: any[] }, _pages: any) =>
+  getNextPageParam: (lastPage: { nextCursor: any; products: any[] }, _pages: any) =>
     lastPage.nextCursor ?? undefined, // maxPages: 6
 })
 
